96155d20c7df244f50eb40cb12c5fe987d7c41cb
CesarExpositoOvalle <cesarexpositoovalle@gmail.com>
2025-10-09 19:30:59 +0200

    archivos url django

--- Django/djangoproject/myapp/views.py
+++ Django/djangoproject/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("<h2>Hello world!</h2>")
+
+def about(request):
+    return HttpResponse('About')
+
+
