e316ceabf014e79a0d2f75bdcca0f5d1a761b08c
CesarExpositoOvalle <cesarexpositoovalle@gmail.com>
2025-10-09 08:03:38 +0200

    finalizacion conceptos basicos python

---
+++ 13_modulos.py
@@ -0,0 +1,20 @@
+### Modules ###
+
+from math import pi as PI_VALUE
+import math
+from my_modules import sumValue, printValue
+import my_modules
+
+my_modules.sumValue(5, 3, 1)
+my_modules.printValue("Hola Python!")
+
+
+sumValue(5, 3, 1)
+printValue("Hola python")
+
+
+print(math.pi)
+print(math.pow(2, 8))
+
+
+print(PI_VALUE)
