1af4d41eddc191b0df6ab3223b0eed25f1cee956
CesarExpositoOvalle <cesarexpositoovalle@gmail.com>
2025-10-13 09:37:21 +0200

    nuevo projecto djangocrud

--- Django/djangoproject/myapp/views.py
+++ Django/djangoproject/myapp/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CreateNewTask, CreateNewProject
 # Create your views here.
 
@@ -41,7 +40,7 @@ def create_task(request):
     else:
         Task.objects.create(title=request.POST['title'],
         description=request.POST['description'], project_id=2)
-        return redirect('/tasks/')
+        return redirect('tasks')
     
 def create_project(request):
     if request.method == 'GET':
@@ -49,9 +48,13 @@ def create_project(request):
         'form' : CreateNewProject()
         })
     else:
-        print(request.POST)
-        project=Project.objects.create(name=request.POST['name'])
-        print(project)
-        return render(request, 'projects/create_project.html',{
-            'form' : CreateNewProject()
-        })
+        Project.objects.create(name=request.POST['name'])
+        return redirect('projects')
+    
+def project_detail(request,id):
+    project=get_object_or_404(Project,id=id)
+    tasks=Task.objects.filter(project_id=id)
+    return render(request, 'projects/detail.html',{
+        'project':project,
+        'tasks': tasks
+    })
